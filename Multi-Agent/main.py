7adbe8414d982026ed4484ca2dee28169a4724e7
Arun Bala Subramaniyan <bsarun2014@gmail.com>
2020-06-18 16:49:10 -0700

    Add files via upload

---
+++ Multi-Agent/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 14:30:36 2020
+
+@author: Arun
+"""
+
+
+#import Simurgh-multi-agent-main
+from Simurgh_multi_agent_main import mddpg
+import streamlit as st
+
+
+################################
+##                            ##
+##          Arun B S          ##
+##    github.com/arunbalas    ##
+##                            ##
+################################
+
+
+if __name__ == "__main__":
+    st.write("Training Started")
+    scores = mddpg(n_episodes=1500, max_t=1000, print_every=10)
